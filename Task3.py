d578fac9a42e62d10fc0c7d7afbac23040a8f356
JalmaGit <jmbb00@hotmail.com>
2024-02-18 21:14:37 +0100

    Works for now

--- Task3.py
+++ Task3.py
@@ -1,40 +1,68 @@
 import random
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def main():
     # newProblem = Problem(target, population, mutation, crossover_probability)
-    newProblem = Problem("Joar_Breivik*588289", 500, 0.05, 0.8)
+    newProblem = Problem("Joar_Breivik*588289", 50, 0.05, 0.8)
     newProblem.generation = 1000
+
     print(newProblem.run_genetic_algorithm())
+
+    x = np.linspace(0,newProblem.generation,newProblem.generation+1)
+    y = newProblem.best_fitness
+
+    if (len(x)!=newProblem.generation-1):
+        for i in range(len(x) - len(y)):
+            y.append(newProblem.best_fitness_level)
+
+    plt.title("Fitness Performance Graph, Solution Found: " + str(newProblem.target_lock) + "\n Target: " + newProblem.target_name
+              + ", Pop: " + str(newProblem.pop) + ", Pm: " + str(newProblem.mutation)
+             + ", Pc: " + str(newProblem.crossover_probability))
+    plt.xlabel("Generations")
+    plt.ylabel("Fitness Chromosome")
+
+    plt.plot(x, y)
+    plt.show()
+
 
 def DNA():
     characters = " ABCDEFGHIJKLMNOPQRTSUVWXYZØÆÅabcdefghijklmnopqrstuvwxyzæøå#*_-^<>:;@.,123456789"
     char = random.choice(characters)
     return char
 
+
 class Individual:
-    def __init__(self,name_length):
+    def __init__(self, name_length):
         self.name = []
         self.weight = 1
 
-        #Creates the name for the indivual
+        # Creates the name for the indivual
         for i in range(name_length):
             self.name.append(DNA())
 
+
 class Problem:
-    def __init__(self, target_name, pop, mutation,crossover_probability):
+    def __init__(self, target_name, pop, mutation, crossover_probability):
 
         self.mutation = mutation
         self.crossover_probability = crossover_probability
         self.target_name = target_name
         self.target_length = len(target_name)
+        self.target_lock = False
 
         self.generation = 0
         self.generations_to_found = 0
 
-        self.weight_multiplier = 10
-        self.best_fitness = 1
+        self.best_fitness = []
+        self.best_fitness_level = 0
+
+        self.weight_multiplier = 5
+        self.best_current_fitness = 1
         self.best = ""
 
+        self.pop = pop
         self.population = []
 
         for i in range(pop):
@@ -44,18 +72,21 @@ class Problem:
     def fitness_test(self):
         for i in range(len(self.population)):
             self.population[i] = self.weight_calculation(self.population[i])
+        self.best_fitness.append(self.best_fitness_level)
 
     def weight_calculation(self, current_individual):
         current_name = ""
+        current_fitness = 0
 
         for i in range(self.target_length):
             if current_individual.name[i] == self.target_name[i]:
                 current_individual.weight += self.weight_calc_func(current_individual.weight)
+                current_fitness += 1
             current_name += current_individual.name[i]
 
-
-        if self.best_fitness <= current_individual.weight:
-            self.best_fitness = current_individual.weight
+        if self.best_current_fitness <= current_individual.weight:
+            self.best_current_fitness = current_individual.weight
+            self.best_fitness_level = current_fitness/self.target_length
             self.best = current_name
 
         return current_individual
@@ -65,9 +96,9 @@ class Problem:
 
     def mating(self):
         new_population = []
-        for i in range(len(self.population)//2):
-            parent1,parent2 = self.weighted_random_selection()
-            child1,child2 = self.single_point_crossover(parent1,parent2)
+        for i in range(len(self.population) // 2):
+            parent1, parent2 = self.weighted_random_selection()
+            child1, child2 = self.single_point_crossover(parent1, parent2)
             new_population.append(child1)
             new_population.append(child2)
 
@@ -83,11 +114,11 @@ class Problem:
             weights.append(self.population[i].weight)
             listOfNames.append(self.population[i].name)
 
-        #Randomly selects name, weighted towards name closest to the solution
+        # Randomly selects name, weighted towards name closest to the solution
         parent1 = random.choices(listOfNames, weights)[0]
         parent2 = random.choices(listOfNames, weights)[0]
 
-        return parent1,parent2
+        return parent1, parent2
 
     def single_point_crossover(self, parent1, parent2):
         child1 = []
@@ -112,9 +143,9 @@ class Problem:
 
         return child1, child2
 
-    def mutate_child(self,child):
+    def mutate_child(self, child):
         for i in range(self.target_length):
-            if random.uniform(0,1) <= self.mutation:
+            if random.uniform(0, 1) <= self.mutation:
                 child[i] = DNA()
         return child
 
@@ -127,7 +158,9 @@ class Problem:
             print(i)
             print(self.best)
             if self.best == self.target_name:
+                self.target_lock = True
                 return "Target name found, " + self.best + " With " + str(i) + " generations"
         return "Target not found"
+
 
 main()
